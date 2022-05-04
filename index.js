aecf156522d99d505296503edf44881070ec80e1
Oyeinbomo <oyeinx7@gmail.com>
2022-05-04 12:21:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ let counter = document.getElementById('count');
 let decreaseBtn = document.getElementById('decrease-btn');
 let resetBtn = document.getElementById('reset-btn');
 let increaseBtn = document.getElementById('increase-btn');
+let saveBtn = document.getElementById('save-btn')
+let prevCount = document.getElementById('prev-counts')
+let clrPrevCount = document.getElementById('clear-prev-count')
 
 decreaseBtn.addEventListener('click', () => {
-    counter.textContent--;
+    count.textContent--;
 })
 
 resetBtn.addEventListener('click', () => {
-    counter.textContent = 0;
+    count.textContent = 0;
 })
 
 increaseBtn.addEventListener('click', () => {
-    counter.textContent++;
+    count.textContent++;
+})
+
+saveBtn.addEventListener('click', function () {
+    let currentCount = counter.textContent
+
+    prevCount.textContent += ` ${currentCount} `
+    counter.textContent = 0
+})
+
+clrPrevCount.addEventListener('click', function () {
+    prevCount.textContent = ''
 })
